14777613f80f6620b017609f5384fb3b7aee1ae6
SaulCastaneda1343 <saboo.leon@gmail.com>
2025-05-12 19:57:37 -0700

    Did Some UI and Particle Effects

---
+++ Scastaneda_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    /* increased start speed */
+    /* increased start size to 10 */
+    /* changed start color to blue */
+    /* set max particles to 20000 */
+    /* added color over lifetime */
+    /* added color over lifetime */
+}
---
+++ Scastaneda_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,3 @@
+/* Added a title with different colors and type of text */
+/* Added a different background */
+/* Added two different buttons in the UI */
