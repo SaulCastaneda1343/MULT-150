a01a07cb4096334a9deb1b4e23f5b19589ee5496
SaulCastaneda1343 <saboo.leon@gmail.com>
2025-05-31 14:58:39 -0700

    Final Assignment

---
+++ Scastaneda_FFinal/Assets/Modificatiion.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class SceneModifications : MonoBehaviour
+{
+    void Start()
+    {
+        /* 1 - Directional Light: changed to blue and 1.5 intensity */
+
+        /* 2 - Added a white fog with 0.008 density */
+
+        /* 3 - Changed the skybox to a custom pink sky tint with 1.39 exposure and 5 convergence */
+
+        /* 4 - Added a second chaos ball with particle effects and different sizes */
+
+        /* 5 - Changed colored balls to have 1 on both metallic and smoothness, and added a halo effect in their color */
+
+        /* 6 - Changed the range of all goals to 20 */
+
+        /* 7 - Completely removed the old walls and replaced them with a new structure, updated color and collision settings */
+
+        /* 8 - Added optional sand terrain in the background as an extra visual element */
+    }
+}
---
+++ Scastaneda_FFinal/Assets/Scripts/Gamecritic.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamecritic : MonoBehaviour
+{
+    /* 
+    Is the game fun?
+    Yes, the game is simple but fun it's satisfying to solve hit the balls goals and it is simple but entertaining.
+
+    What would make the game more fun?
+    Adding sound effects, particle effects when you hit the goals could help, and to adding a time counter would make it more fun as well. 
+
+    What would make the game easier to play?
+    showing the player which goals remain, and adding color indicators or UI would help in my opinion.
+
+    What parts of the game are confusing?
+    I do not think any part of the game is confusing on the contrary the game works and it is fun because how simple it is. 
+    Im planning to add the following
+    Try adding a timer so that the player knows how long it took to win.
+
+Try adding a chaos goal that you must bounce all the chaos balls into.
+
+Try changing the size or shape of the player's bumper.
+
+Try changing the footsteps audio to something that makes more sense with the water.
+
+    */
+}
